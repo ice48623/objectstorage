04e40146a9adedc27ad059b761417f4f9681e736
ice48623 <ice48623@gmail.com>
2018-09-15 16:28:17 +0700

    add metadata repository

---
+++ src/main/java/com/muic/objectstorage/Repository/MetadataRepository.java
@@ -0,0 +1,10 @@
+package com.muic.objectstorage.Repository;
+
+import com.muic.objectstorage.Entity.Metadata;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MetadataRepository extends CrudRepository<Metadata, Integer> {
+    Metadata findByName(String name);
+}
