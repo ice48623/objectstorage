36b15fdc02cb2bc53b924782cf0f820e8f897d10
ice48623 <ice48623@gmail.com>
2018-09-16 14:28:50 +0700

    change to CRUD

--- src/main/java/com/muic/objectstorage/Repository/MetadataRepository.java
+++ src/main/java/com/muic/objectstorage/Repository/MetadataRepository.java
@@ -2,13 +2,12 @@ package com.muic.objectstorage.Repository;
 
 import com.muic.objectstorage.Entity.Metadata;
 import com.muic.objectstorage.Entity.ObjectMetadataComposite;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MetadataRepository extends JpaRepository<Metadata, ObjectMetadataComposite> {
+public interface MetadataRepository extends CrudRepository<Metadata, ObjectMetadataComposite> {
     List<Metadata> findByObjectId(Integer objectId);
 }
