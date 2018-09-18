9587f0fc84357485f015a420bef4c9a47a4bc743
ice48623 <ice48623@gmail.com>
2018-09-18 17:03:45 +0700

    add ETag bean

--- src/main/java/com/muic/objectstorage/ObjectstorageApplication.java
+++ src/main/java/com/muic/objectstorage/ObjectstorageApplication.java
@@ -2,11 +2,20 @@ package com.muic.objectstorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class ObjectstorageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ObjectstorageApplication.class, args);
+	}
+
+	@Bean
+	public Filter filter(){
+		ShallowEtagHeaderFilter filter = new ShallowEtagHeaderFilter();
+		return filter;
 	}
 }
